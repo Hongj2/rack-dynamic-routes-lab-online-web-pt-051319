c502c2774cb78ab1cfbaaa4472acd6409f0a45d8
Jessica Hong <jessicahong.ngnr@gmail.com>
2019-08-10 18:13:18 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -8,7 +8,7 @@ class Application
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last
       item= @@items.find{|i| i.name == item_name}
-      if item.nill?
+      if item.nil?
         resp.write "error"
         resp.status = 400
       else
