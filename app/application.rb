f50b47c91751d5e754fd174a63edb9cc69a63765
Jessica Hong <jessicahong.ngnr@gmail.com>
2019-08-10 18:05:21 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,4 +1,13 @@
 class Application
   @@items = [] 
   
+  def call(env)
+    resp = Rake::Response.new
+    req  = Rake::Request.new(env)
+    
+    if req.path.match(/items/)
+      
+    else
+      resp.write 
+  
   
