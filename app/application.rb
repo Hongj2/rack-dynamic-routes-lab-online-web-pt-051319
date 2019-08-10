a5cdb18e28791370f9b83c301cdcd164d90086bb
Jessica Hong <jessicahong.ngnr@gmail.com>
2019-08-10 18:08:47 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -6,8 +6,19 @@ class Application
     req  = Rake::Request.new(env)
     
     if req.path.match(/items/)
+      item = req.parms["item"]
+      if items.include?(item)
+        item.price
+      else
+        resp.write "error"
+        resp.status = 400
+      end
       
     else
-      resp.write 
-  
+      resp.write "Error"
+      resp.status = 404
+    end
+    resp.finish
+  end
+end
   
